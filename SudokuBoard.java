d557ceeeae004e23763261a9151719dee7166dfb
NathanielDelamarter <nathaniel.delamarter@go.shoreline.edu>
2025-04-16 12:46:52 -0700

    Part 2: completed constructor
    Initialized the constructor to read in board files and save them to the board array.

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,11 +1,24 @@
 import java.util.*;
+import java.io.*;
 
 public class SudokuBoard{
 
-   private char[][];
+   private char[][] board;
    
-   public SudokuBoard(String filname){
-      
+   public SudokuBoard(String filename){
+      try {
+         Scanner file = new Scanner(new File(filename));
+         int row = 0;
+         while(file.hasNextLine()){
+            String s = file.nextLine();
+            for(int i = 0; i < s.length(); i++){
+               board[row][i] = s.charAt(i);
+            }
+            row++;
+         }
+      }catch (FileNotFoundException e){
+         System.out.println("File does not exist");
+      }
    }
    
    public String toString(){
