57cae0caa565cd5cea2907929f9a9dd1465085e9
NathanielDelamarter <nathaniel.delamarter@go.shoreline.edu>
2025-04-16 12:19:04 -0700

    Add files via upload

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -2,4 +2,14 @@ import java.util.*;
 
 public class SudokuBoard{
 
+   private char[][];
+   
+   public SudokuBoard(String filname){
+      
+   }
+   
+   public String toString(){
+      return "";
+   }
+   
 }
