08eb8140fdfd437795c6f9129a8326ed9c7bc48b
NathanielDelamarter <nathaniel.delamarter@go.shoreline.edu>
2025-04-17 08:53:22 -0700

    Created SudokuTest File

---
+++ SudokuTest.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class SudokuTest{
+   public static void main(String[] args){
+      SudokuBoard board = new SudokuBoard("data1.sdk");
+      System.out.println(board.toString());
+   }
+}
